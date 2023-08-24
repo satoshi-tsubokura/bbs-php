476968f606f394de594120d8d38bf679ce6bc446
satoshi-tsubokura <tsubokurajob151718@gmail.com>
2023-08-24 22:23:38 +0900

    #8 コメント一覧画面作成

--- app/views/pages/board.php
+++ app/views/pages/board.php
@@ -4,54 +4,61 @@
   <div class="l-inner">
     <section class="c-section p-board">
       <!-- Todoコメント一覧 -->
-      <h2 class="c-section__ttl"><?= $board->getTitle() ?></h2>
-      <p class="p-board__description"><?= $board->getDescription() ?></p>
+      <div class="p-board__heading">
+        <h2 class="c-section__ttl p-board__ttl"><?= $board->getTitle() ?></h2>
+        <p class="p-board__description"><?= $board->getDescription() ?></p>
+      </div>
+    <?php
+      if(count($comments) === 0) {
+          ?>
+    <p>まだコメントはありません。</p>
+    <?php
+      }
+?>
     <ul class="p-board__comments">
       <?php
-        foreach($comments as $comment) {
-            ?>
-        <li class="c-comment">
-          <div class="c-comment__meta">
-            <span class="c-comment__no"><?= $comment->getCommentNo() ?>.</span>
-            <span class="c-comment_user-name"><?= $comment->getUser()->getUserName() ?>さん</span>
-            <span class="c-comment__date"><?= $comment->getUpdatedAt()->format("Y年m月d日 h:i:s") ?></span>
+    foreach($comments as $comment) {
+        ?>
+        <li class="p-comment">
+          <div class="p-comment__meta">
+            <span class="p-comment__no"><?= $comment->getCommentNo() ?>.</span>
+            <span class="p-comment_user-name">名前: <?= $comment->getUser()->getUserName() ?>さん</span>
+            <span class="p-comment__date"><?= $comment->getUpdatedAt()->format("Y年m月d日 h:i:s") ?></span>
           </div>
-          <p class="c-comment__body"><?= $comment->getCommentBody() ?></p>
+          <p class="p-comment__body"><?= $comment->getCommentBody() ?></p>
         </li>
       <?php
-        }
+    }
 ?>  
     </ul>
 
-    </section>
-    <!-- c-section p-board -->
-      <form method="POST" action="/board/<?= $boardId ?>" enctype="multipart/form-data">
-        <!-- エラーメッセージ(フォーム全体) -->
-        <?php if(isset($errorMsgs['messages'])) { ?>
-          <ul class="p-board-form__errors">
-          <?php foreach($errorMsgs['messages'] as $msg) { ?>
-            <li class="c-error-msg"><?= $msg ?></li>
-          <?php
-          }
-            ?>
-          </ul>
+    <form method="POST" action="/board/<?= $boardId ?>" enctype="multipart/form-data">
+      <!-- エラーメッセージ(フォーム全体) -->
+      <?php if(isset($errorMsgs['messages'])) { ?>
+        <ul class="p-board-form__errors">
+        <?php foreach($errorMsgs['messages'] as $msg) { ?>
+          <li class="c-error-msg"><?= $msg ?></li>
         <?php
         }
+          ?>
+        </ul>
+      <?php
+      }
 ?>
         <dl class="p-board-form__fields">
           <dt class="p-board-form__label"><label for="comment-body">コメント</label></dt>
           <dd class="p-board-form__input">
             <textarea type="text" name="comment" id="comment-body" cols="100" rows="10" required maxlength="1000"><?= $originValues['comment'] ?? '' ?></textarea>
-             <?php if(isset($errorMsgs['comment'])) { ?>
+            <?php if(isset($errorMsgs['comment'])) { ?>
               <ul>
               <?php  foreach($errorMsgs['comment'] as $msg) { ?>
                 <li class="c-error-msg"><?= $msg ?></li>
               <?php
               }
-                 ?>
+                ?>
               </ul>
             <?php
-             }
+            }
 ?>
           </dd>
         </dl>
@@ -59,7 +66,9 @@
         <input type="hidden" name="token" value="<?= $csrfToken ?>">
         <button type="submit" class="c-btn c-btn--primary p-board-form__submit">投稿</button>
       </form>
-      <!-- p-board-form --> 
+    <!-- p-board-form --> 
+    </section>
+    <!-- c-section p-board -->
   </div>
 </body>
 </html>
