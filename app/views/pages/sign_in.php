07e682bb60c8b38e40b4a123ae45acaa51d606a2
satoshi-tsubokura <tsubokurajob151718@gmail.com>
2023-08-26 18:47:13 +0900

    ログイン画面表示時の修正

--- app/views/pages/sign_in.php
+++ app/views/pages/sign_in.php
@@ -21,7 +21,7 @@
             <label for="input_name">ユーザー名</label>
           </dt>
           <dd class="p-signinup__input">
-            <input type="text" name="name" id="input_name" class="p-signinup__item" size="25" required minlength="3" maxlength="20" value="<?= $parameters['name'] ?? '' ?>">
+            <input type="text" name="name" id="input_name" class="p-signinup__item" size="25" required minlength="3" maxlength="20" value="<?= $originValues['name'] ?? '' ?>">
             <?php if(isset($errorMsgs['name'])) { ?>
               <ul>
               <?php foreach($errorMsgs['name'] as $msg) { ?>
@@ -38,7 +38,7 @@
             <label for="input_password">パスワード</label>
           </dt>
           <dd class="p-signinup__input">
-            <input type="password" name="password" id="input_password" class="p-signinup__item" size="30" required minlength="10" maxlength="72" value="<?= $parameters['password'] ?? '' ?>">
+            <input type="password" name="password" id="input_password" class="p-signinup__item" size="30" required minlength="10" maxlength="72" value="<?= $originValues['password'] ?? '' ?>">
             <?php if(isset($errorMsgs['password'])) { ?>
               <ul>
               <?php foreach($errorMsgs['password'] as $msg) { ?>
